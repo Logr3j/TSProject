53917f37432038dc04e1a0ea435a7ebc30a9f85b
Milorad Miletić <mmiletic@gmail.com>
2021-10-10 13:40:05 +0200

    Small corrections.

--- App.tsx
+++ App.tsx
@@ -1,160 +1,150 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet  } from "react-native";
-import { Button } from "react-native-paper";
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+import {Button} from 'react-native-paper';
 
-
-
-
-const AlignSelfLayout = () => {
-  const [ setAlignSelf ] = useState("stretch");
+function index() {
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
+
   return (
-    <PreviewLayout 
-      label="Stoni tenis"            
-      setSelectedValue={setAlignSelf}>       
-       <View style={styles.row}>  
-        <View
-            style={styles.box}>
-            <Text style={styles.bigBlue}>{count1}</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.row}>
+        <View style={styles.box}>
+          <Text style={styles.bigBlue}>{count1}</Text>
         </View>
-        <View
-            style={styles.box}>
-            <Text style={styles.bigBlue}>{count2}</Text>
+        <View style={styles.box}>
+          <Text style={styles.bigBlue}>{count2}</Text>
         </View>
       </View>
-      <View style={styles.row}>  
-          <View style={styles.buttonContainer}>
-            <Button 
-            color="green" 
-            mode="contained" 
-            onPress={() => setCount1(count1 + 1)
-            }>
-                Prvi igrač
-            </Button>
-          </View>        
+      <View style={styles.row}>
+        <View style={styles.row}>
+          <View style={styles.box}>
+            {(() => {
+              if ((count1 + count2) % 4 == 0 || (count1 + count2) % 4 == 1) {
+                return <Text style={styles.bigBlue}>{'SERVE'}</Text>;
+              }
+              return null;
+            })()}
+          </View>
+          <View style={styles.box}>
+            {(() => {
+              if ((count1 + count2) % 4 == 2 || (count1 + count2) % 4 == 3) {
+                return <Text style={styles.bigBlue}>{'SERVE'}</Text>;
+              }
+              return null;
+            })()}
+          </View>
+        </View>
+
         <View style={styles.buttonContainer}>
-           <Button 
+          <Button
             color="green"
-            mode="contained" 
+            mode="contained"
+            onPress={() => setCount1(count1 + 1)}>
+            PLAYER1 +
+          </Button>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            color="green"
+            mode="contained"
             onPress={() => setCount2(count2 + 1)}>
-                Drugi igrač
-            </Button>
-          </View>
-          </View>
-     
-      <View style={styles.row}>  
-        <View
-            style={styles.box}>
-            {(() => {
-              if (((count1+count2)%4 == 0) || ((count1+count2)%4 == 1)){
-                  return (
-            <Text style={styles.bigBlue}>{'servis'}</Text>
-            )
-              }
-              
-              return null;
-            })()}
-            </View>
-        <View
-           style={styles.box}>
-            {(() => {
-              if (((count1+count2)%4 == 2) || ((count1+count2)%4 == 3)){
-                  return (
-            <Text style={styles.bigBlue}>{'servis'}</Text>
-            )
-              }
-              
-              return null;
-            })()}
+            PLAYER2 +
+          </Button>
         </View>
       </View>
-      
-      <View style={styles.row}>  
-          <View style={styles.buttonContainer}>
-            <Button 
-            color="orange" 
-            mode="contained" 
-            onPress={() => setCount1(count1 - 1)
-            }>
-                Poništi poen
-            </Button>
-          </View>        
+
+      <View style={styles.row}>
         <View style={styles.buttonContainer}>
-           <Button 
+          <Button
             color="orange"
-            mode="contained" 
+            mode="contained"
+            onPress={() => setCount1(count1 - 1)}>
+            PLAYER1 -
+          </Button>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            color="orange"
+            mode="contained"
             onPress={() => setCount2(count2 - 1)}>
-                poništi poen
-            </Button>
-          </View>
-          </View>
-    </PreviewLayout>
+            PLAYER2 -
+          </Button>
+        </View>
+      </View>
+
+      <View style={styles.row}>
+        <View style={styles.buttonContainer}>
+          <Button
+            color="#BF4D28"
+            mode="contained"
+            onPress={() => {
+              setCount1(0);
+              setCount2(0);
+            }}>
+            reset
+          </Button>
+        </View>
+      </View>
+    </SafeAreaView>
   );
-};
+}
 
-
-const PreviewLayout = ({
-  label,
-  children,
-}) => (
-  <View style={{padding: 10, flex: 1, }}>
+const PreviewLayout = ({label, children}) => (
+  <View style={{padding: 10, flex: 1}}>
     <Text style={styles.label}>{label}</Text>
-    
-    <View style={styles.container}>
-      {children}
-    </View>
+
+    <View style={styles.container}>{children}</View>
   </View>
 );
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "lightgreen",
-    minHeight: 200,
+    backgroundColor: 'lightgreen',
+    padding: 10,
   },
   box: {
     paddingHorizontal: 8,
     paddingVertical: 6,
     borderRadius: 4,
-    backgroundColor: "white",
-    alignSelf: "flex-start",
-    marginHorizontal: "1%",
+    backgroundColor: 'white',
+    alignSelf: 'flex-start',
+    marginHorizontal: '1%',
     marginBottom: 6,
-    minWidth: "48%",
-    textAlign: "center",
+    minWidth: '48%',
+    textAlign: 'center',
   },
   row: {
-    flexDirection: "row",
-    flexWrap: "wrap",
+    flexDirection: 'row',
+    flexWrap: 'wrap',
     marginTop: 10,
   },
   button: {
     paddingHorizontal: 8,
     paddingVertical: 6,
     borderRadius: 4,
-    backgroundColor: "pink",
-    alignSelf: "flex-start",
-    marginHorizontal: "1%",
+    backgroundColor: 'pink',
+    alignSelf: 'flex-start',
+    marginHorizontal: '1%',
     marginBottom: 6,
-    minWidth: "48%",
-    textAlign: "center",
+    minWidth: '48%',
+    textAlign: 'center',
   },
   selected: {
-    backgroundColor: "red",
+    backgroundColor: 'red',
     borderWidth: 0,
   },
   buttonLabel: {
     fontSize: 12,
-    fontWeight: "500",
-    color: "black",
+    fontWeight: '500',
+    color: 'black',
   },
   selectedLabel: {
-    color: "white",
+    color: 'white',
   },
   label: {
-    textAlign: "center",
+    textAlign: 'center',
     marginBottom: 10,
     fontSize: 24,
   },
@@ -162,21 +152,20 @@ const styles = StyleSheet.create({
     color: 'black',
     fontWeight: 'bold',
     fontSize: 30,
-    textAlign: "center",
+    textAlign: 'center',
   },
   buttonContainer: {
     margin: 10,
-    marginHorizontal: "1%",
-    minWidth: "48%",
-    
-  },  
+    marginHorizontal: '1%',
+    minWidth: '48%',
+  },
   text: {
     borderWidth: 1,
     padding: 10,
-    minWidth: "48%",
+    minWidth: '48%',
     borderColor: 'black',
-    backgroundColor: 'pink'
- }
+    backgroundColor: 'pink',
+  },
 });
 
-export default AlignSelfLayout;
+export default index;
