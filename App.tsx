4266278704ffc2ff5d2fe01396b4ac888fca8c84
Milorad Miletić <mmiletic@gmail.com>
2021-11-30 22:03:05 +0100

    Buttons hiding

--- App.tsx
+++ App.tsx
@@ -1,179 +1,105 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 import {Button} from 'react-native-paper';
 
-function index() {
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
+const Stopwatch = () => {
+  const [time, setTime] = React.useState(0);
+  const [timerOn, setTimerOn] = React.useState(false);
+
+  React.useEffect(() => {
+    let interval = null;
+
+    if (timerOn) {
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 100);
+      }, 100);
+    } else if (!timerOn) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [timerOn]);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.row}>
-        <View style={styles.box}>
-          <Text style={styles.bigBlue}>{count1}</Text>
+    <View style={styles.container}>
+      <View style={styles.containerClock}>
+        <Text style={styles.digits}>{('0' + Math.floor((time / 60000) % 60)).slice(-2)}</Text>
+        <Text style={styles.dots}>:</Text>
+        <Text style={styles.digits}>{('0' + Math.floor((time / 1000) % 60)).slice(-2)}</Text>
+        <Text style={styles.dots}>:</Text>
+        <Text style={styles.digits}>{('0' + ((time / 100) % 100)).slice(-1)}</Text>
         </View>
-        <View style={styles.box}>
-          <Text style={styles.bigBlue}>{count2}</Text>
-        </View>
+      <View style={styles.containerButton}>
+        {!timerOn && time === 0 && (<Button
+          style={styles.button}
+          color={'yellow'}
+          labelStyle={{ color: 'black', fontSize: 10 }}
+          mode="contained"
+          onPress={() => setTimerOn(true)}>
+          Start
+        </Button>)}
+        {timerOn && <Button
+          style={styles.button}
+          color={'yellow'}
+          labelStyle={{ color: 'black', fontSize: 10 }}
+          mode="contained"
+          onPress={() => setTimerOn(false)}>
+          Stop
+        </Button>}
+        {!timerOn && time > 0 && (<Button
+          style={styles.button}
+          color={'yellow'}
+          labelStyle={{ color: 'black', fontSize: 10 }}
+          mode="contained"
+          onPress={() => setTimerOn(true)}>
+          Resume
+        </Button>)}
+        {!timerOn && time > 0 && (<Button
+          style={styles.button}
+          color={'yellow'}
+          labelStyle={{ color: 'black', fontSize: 10 }}
+          mode="contained"
+          onPress={() => setTime(0)}>
+          Reset
+        </Button>)}
       </View>
-      <View style={styles.row}>
-        <View style={styles.row}>
-          <View style={styles.box}>
-            {(() => {
-              if (count1 + count2 >= 22) {
-                if ((count1 + count2) % 2 == 1) {
-                  return <Text style={styles.bigBlue}>{'SERVE'}</Text>;
-                }
-                return null;
-              }
-              {
-                if ((count1 + count2) % 4 == 0 || (count1 + count2) % 4 == 1) {
-                  return <Text style={styles.bigBlue}>{'SERVE'}</Text>;
-                }
-                return null;
-              }
-            })()}
-          </View>
-          <View style={styles.box}>
-            {(() => {
-              if (count1 + count2 >= 22) {
-                if ((count1 + count2) % 2 == 0) {
-                  return <Text style={styles.bigBlue}>{'SERVE'}</Text>;
-                }
-                return null;
-              }
-              {
-                if ((count1 + count2) % 4 == 2 || (count1 + count2) % 4 == 3) {
-                  return <Text style={styles.bigBlue}>{'SERVE'}</Text>;
-                }
-              }
-              return null;
-            })()}
-          </View>
-        </View>
-
-        <View style={styles.buttonContainer}>
-          <Button
-            color="green"
-            mode="contained"
-            onPress={() => setCount1(count1 + 1)}>
-            PLAYER1 +
-          </Button>
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            color="green"
-            mode="contained"
-            onPress={() => setCount2(count2 + 1)}>
-            PLAYER2 +
-          </Button>
-        </View>
-      </View>
-
-      <View style={styles.row}>
-        <View style={styles.buttonContainer}>
-          <Button
-            color="orange"
-            mode="contained"
-            onPress={() => setCount1(count1 - 1)}>
-            PLAYER1 -
-          </Button>
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            color="orange"
-            mode="contained"
-            onPress={() => setCount2(count2 - 1)}>
-            PLAYER2 -
-          </Button>
-        </View>
-      </View>
-
-      <View style={styles.row}>
-        <View style={styles.buttonContainer}>
-          <Button
-            color="#BF4D28"
-            mode="contained"
-            onPress={() => {
-              setCount1(0);
-              setCount2(0);
-            }}>
-            reset
-          </Button>
-        </View>
-      </View>
-    </SafeAreaView>
+    </View>
   );
-}
+};
+
+export default Stopwatch;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'lightgreen',
-    padding: 10,
+    alignItems: 'center',
+    backgroundColor: 'gray',
   },
-  box: {
-    paddingHorizontal: 8,
-    paddingVertical: 6,
-    borderRadius: 4,
-    backgroundColor: 'white',
-    alignSelf: 'flex-start',
-    marginHorizontal: '1%',
-    marginBottom: 6,
-    minWidth: '48%',
-    textAlign: 'center',
-  },
-  row: {
+  containerClock: {
     flexDirection: 'row',
-    flexWrap: 'wrap',
-    marginTop: 10,
+    height: 100,
+    alignItems: 'center',
+  },
+  containerButton: {
+    flexDirection: 'row',
+  },
+  digits: {
+    backgroundColor: 'lightgray',
+    width: 60,
+    height: 60,
+    fontSize: 40,
+    alignItems: 'center',
+    textAlign: 'center',
+    borderRadius: 10,
+    borderColor: 'darkgray',
+    borderWidth: 2,
+  },
+  dots: {
+    fontSize: 40,
+    width: 15,
+  textAlign: 'center',
   },
   button: {
-    paddingHorizontal: 8,
-    paddingVertical: 6,
-    borderRadius: 4,
-    backgroundColor: 'pink',
-    alignSelf: 'flex-start',
-    marginHorizontal: '1%',
-    marginBottom: 6,
-    minWidth: '48%',
-    textAlign: 'center',
-  },
-  selected: {
-    backgroundColor: 'red',
-    borderWidth: 0,
-  },
-  buttonLabel: {
-    fontSize: 12,
-    fontWeight: '500',
-    color: 'black',
-  },
-  selectedLabel: {
-    color: 'white',
-  },
-  label: {
-    textAlign: 'center',
-    marginBottom: 10,
-    fontSize: 24,
-  },
-  bigBlue: {
-    color: 'black',
-    fontWeight: 'bold',
-    fontSize: 30,
-    textAlign: 'center',
-  },
-  buttonContainer: {
-    margin: 10,
-    marginHorizontal: '1%',
-    minWidth: '48%',
-  },
-  text: {
-    borderWidth: 1,
-    padding: 10,
-    minWidth: '48%',
-    borderColor: 'black',
-    backgroundColor: 'pink',
+    width: '20%',
   },
 });
-
-export default index;
